23562577d0780a625077711d17002a9b1a2bfbc9
EanDalin0012 <44183364+EanDalin0012@users.noreply.github.com>
2021-09-30 17:25:22 +0700

    Commit

--- src/main/java/com/evideo/evideobackend/web/ReadVD.java
+++ src/main/java/com/evideo/evideobackend/web/ReadVD.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,9 +17,14 @@ import java.io.InputStream;
 @RequestMapping(value = "/unsecur/api/read/vd")
 public class ReadVD {
 
-    @GetMapping(value = "/v0")
-    public ResponseEntity LInputStreamResourceretrieveResource() throws Exception {
-        File file = new File("G:\\vedio\\flutter\\Afgprogrammer\\Flutter Animation\\Flutter Animation Tutorial  Make Page Transition - day 6.mp4");
+    @GetMapping(value = "/v0/{vdID}")
+    public ResponseEntity LInputStreamResourceretrieveResource(@PathVariable("vdID") int vdID) throws Exception {
+        String path = "G:\\vedio\\flutter\\Afgprogrammer\\Flutter Animation\\Flutter Animation Tutorial  Make Page Transition - day 6.mp4";
+        if (vdID >= 1) {
+            path = "G:\\vedio\\flutter\\Afgprogrammer\\Flutter Login Page UI Design\\Flutter UI Tutorial  Login Page UI and Animation - day 12.mp4";
+        }
+
+        File file = new File(path);
         InputStream inputStream = new FileInputStream(file);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Accept-Ranges", "bytes");
